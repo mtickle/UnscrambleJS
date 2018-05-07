301d9e50896632738a68e9dcbe7d8c59189e5e68
Mike <mtickle@gmail.com>
2018-05-07 11:42:39 -0400

    initi2

--- wordgame.js
+++ wordgame.js
@@ -1,18 +1,23 @@
 $("#btnGenerate").click(function () {
 
+  //--- -----------------------------------------------------------
+  //--- This happens when the "Get New Word" button is clicked.
+  //--- -----------------------------------------------------------
+
+  //--- Reset the timer.
   $("#DateCountdown").TimeCircles().stop();
   $("#DateCountdown").TimeCircles().restart();
 
-  //--- clear the letter board
+  //--- Clear the letter board
   $("#letters").empty();
 
   //--- Get a random number.
   var randomNumber = getNumber();
 
-  //--- Get a word
+  //--- Get a word based in randomNumber
   var randomWord = getWord(randomNumber);
 
-  //--- Try to shuffle it here
+  //--- Shuffle the word.
   var arrShuffledWord = randomWord.split('');
   var shuffledWord = shuffle(arrShuffledWord);
   shuffledWord = shuffledWord.join('');
@@ -20,50 +25,73 @@ $("#btnGenerate").click(function () {
   //--- How long is the word?
   var wordLength = shuffledWord.length;
 
-  //--- Display the word.
+  //--- Display the word in the debug section.
   $('#lblRandomWord').html(randomWord);
-  $('#lblLetterCount').html(wordLength);
-  //--- Reset the keyboard
-  //$("li").removeClass("deaditem");
 
-  //--- Split the word into an array
+  //--- Display the length of the word.
+  $('#lblLetterCount').html(wordLength);
+
+  //--- Split the word into an array.
   var wordArray = [];
   for (var i = 0; i < shuffledWord.length; i++) {
     wordArray.push(shuffledWord.charAt(i));
   }
-  //--- add the letters to the wordboard
+
+  //--- And add the letters to the wordboard
   for (var j = 0; j < wordArray.length; j++) {
     $("#letters").append("<li>" + wordArray[j] + "</li>");
   }
 
+  //--- Start the timer.
   $("#DateCountdown").TimeCircles().start();
 
 });
 
 $("#btnShuffle").click(function () {
+
+  //--- -----------------------------------------------------------
+  //--- This happens when the "Shuffle" button is clicked.
+  //--- -----------------------------------------------------------
+
+  //--- Clear out the wordboard.
   $("#letters").empty();
+
+  //--- Get the word from the debug information
   var randomWord = $('#lblRandomWord').html();
+
+  //--- Shuffle the word.
   var arrShuffledWord = randomWord.split('');
   var shuffledWord = shuffle(arrShuffledWord);
   shuffledWord = shuffledWord.join('');
+
   //--- Split the word into an array
   var wordArray = [];
   for (var i = 0; i < shuffledWord.length; i++) {
     wordArray.push(shuffledWord.charAt(i));
   }
+
   //--- add the letters to the wordboard
   for (var j = 0; j < wordArray.length; j++) {
     $("#letters").append("<li>" + wordArray[j] + "</li>");
   }
+
 });
 
 $("#btnSolve").click(function () {
 
+  //--- -----------------------------------------------------------
+  //--- This happens when the "Shuffle" button is clicked.
+  //--- -----------------------------------------------------------
+
+  //--- Stop the timer
   $("#DateCountdown").TimeCircles().stop();
 
-  var totalTime = 30;
+  //--- Calculate the reward by formatting the remaining seconds 
+  //--- on the timer.
   var elapsedTime = parseInt($("#DateCountdown").TimeCircles().getTime());
   var reward = Math.round(elapsedTime);
+  
+  //--- Display the reward.
   var coinCount = parseInt($('#lblCoinCount').html());
   coinCount = coinCount + reward;
   $('#lblCoinCount').html(coinCount);
@@ -106,10 +134,10 @@ $("#DateCountdown").TimeCircles({
 function shuffle(a) {
   var j, x, i;
   for (i = a.length - 1; i > 0; i--) {
-      j = Math.floor(Math.random() * (i + 1));
-      x = a[i];
-      a[i] = a[j];
-      a[j] = x;
+    j = Math.floor(Math.random() * (i + 1));
+    x = a[i];
+    a[i] = a[j];
+    a[j] = x;
   }
   return a;
 }
@@ -118,10 +146,8 @@ function getNumber() {
   var minNumber = 1;
   var maxNumber = 129582;
   var randomNumber = Math.floor(Math.random() * (maxNumber + 1) + minNumber); // la fonction magique
-
   return randomNumber;
 }
-
 
 function getWord(randomNumber) {
   var word = words[randomNumber];
